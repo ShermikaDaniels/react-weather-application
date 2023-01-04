a0a29d1bc55da27cce4c73cae480a0dcb54c3a19
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2023-01-04 18:29:00 -0500

    added open source GitHub link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import "./App.css";
 
 function App() {
   return <div className="App">Weather App</div>;
+
+  <footer>
+    This project was created by Shermika Daniels and is
+    <a
+      href="https://github.com/ShermikaDaniels/react-weather-application"
+      target="_blank"
+    >
+      {" "}
+      open-sourced{" "}
+    </a>
+    ; on GitHub
+  </footer>;
 }
 
 export default App;
