fb3702627c696d19effdaf8210a4e2da291ef92b
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2023-01-04 18:39:02 -0500

    added the GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
-
-  <footer>
-    This project was created by Shermika Daniels and is
-    <a
-      href="https://github.com/ShermikaDaniels/react-weather-application"
-      target="_blank"
-    >
-      {" "}
-      open-sourced{" "}
-    </a>
-    ; on GitHub
-  </footer>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://www.linkedin.com/in/shermika-daniels-70093b139"
+          target="_blank"
+        >
+          Shermika Daniels
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/ShermikaDaniels/react-weather-application"
+          target="_blank"
+        >
+          {" "}
+          open-sourced{" "}
+        </a>{" "}
+        on GitHub
+      </footer>
+    </div>
+  );
 }
-
-export default App;
