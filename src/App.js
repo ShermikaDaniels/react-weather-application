055ed68b98462ae1734e42ed3596378a46b83fba
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2023-01-04 20:10:45 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://www.linkedin.com/in/shermika-daniels-70093b139"
-          target="_blank"
-        >
-          Shermika Daniels
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/ShermikaDaniels/react-weather-application"
-          target="_blank"
-        >
-          {" "}
-          open-sourced{" "}
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        <h1> Weather App </h1>
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://www.linkedin.com/in/shermika-daniels-70093b139"
+            target="_blank"
+          >
+            Shermika Daniels
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/ShermikaDaniels/react-weather-application"
+            target="_blank"
+          >
+            {" "}
+            open-sourced{" "}
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
