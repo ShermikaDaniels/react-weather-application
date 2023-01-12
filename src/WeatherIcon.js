830da6db3c48778bbd49b02b15d7129730ae2a1f
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2023-01-12 18:39:49 -0500

    building daily forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function WeatherIcon(props) {
-  return <img alt="icon" src={props.code} />;
+  return <img alt="icon" src={props.code} size={props.size} />;
 }
